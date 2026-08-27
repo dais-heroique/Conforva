1d6d9c3f33c5981ea3297ae8a4f58e95d564ecf2
dais-heroique <130305493+dais-heroique@users.noreply.github.com>
2026-08-27 09:48:06 +0200

    Remove competitor management UI

--- src/app/(dashboard)/dashboard/competitors/new/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getLocale } from "@/lib/i18n/locale"
-import { NewCompetitorForm } from "./new-competitor-form"
-
-export default async function NewCompetitorPage() {
-  const locale = await getLocale()
-  return <NewCompetitorForm locale={locale} />
-}
